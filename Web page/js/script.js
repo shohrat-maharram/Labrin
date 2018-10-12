0ec0001d42ca167c4b74d8f305fc3621eca89b50
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2018-10-12 18:11:30 +0400

    Working on web page

--- Web page/js/script.js
+++ Web page/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
 
+    var docHeight = $(document).height();
+    $(window).resize(function () {
+        $("#wrapper").css("height", "" + docHeight + "");
+    });
+
+    $(window).scroll(function () {
+        $("#wrapper").css("height", "" + docHeight + "");
+    });
 
 }); 
